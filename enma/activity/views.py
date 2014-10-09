51bd17d3a648410bb5a86f69353c797f9770ed55
Volker Kempert <volker.kempert@pixmeter.com>
2014-10-09 12:21:27 +0200

    Add pagination for activities

--- enma/activity/views.py
+++ enma/activity/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask.ext.login import login_required, current_user
 
 from enma.activity.models import Activity
@@ -20,16 +20,21 @@ class Columns():
 @login_required
 def home():
     columns = Columns()
-    activities = db.session.query(Activity)
     if current_user.is_administrator():
         columns.actor = True
         columns.act_on = True
         columns.origin = True
+        activities = Activity.query
     else:
-        activities = activities.filter_by(actor=current_user.username)
-    activities = activities.order_by(Activity.timestamp.desc()).all()
+        activities = Activity.query.filter_by(actor=current_user.username)
+    page = request.args.get('page', 1, type=int)
+    pagination = activities.order_by(Activity.timestamp.desc()).paginate(
+                page, per_page=10, error_out=False)
+    print dir(pagination)
     return render_template("activities/list.html", 
-                           activities=activities,
+                           #activities=activities,
+                           activities=pagination.items,
+                           pagination=pagination,
                            columns=columns)
 
 
